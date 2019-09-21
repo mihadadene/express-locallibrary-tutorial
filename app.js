d399aa0fdf77fb2cb395e0da6c8330012daa95e0
mihadadene <mi.ihadadene@gmail.com>
2019-09-21 13:16:00 -0400

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb://aniss.ca:32768/libraryDb';
+var dev_db_url = 'mongodb://aniss.ca:32768/libraryDb';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
